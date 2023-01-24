6c1fd03be4f923cf3565c6ac27b5c3e815fdd3ba
Syamil Abdillah <58422839+syamilAbdillah@users.noreply.github.com>
2023-01-24 18:41:00 +0700

    add domain whitelist

--- controller/middleware.go
+++ controller/middleware.go
@@ -16,7 +16,7 @@ import (
 func cors() func(http.Handler) http.Handler {
 	return chiCors.Handler(chiCors.Options{
 		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
-		AllowedOrigins: []string{"https://*", "http://*"},
+		AllowedOrigins: []string{"https://celadon-mochi-a0b1a6.netlify.app/", "http://*"},
 		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token", "Credentials"},
